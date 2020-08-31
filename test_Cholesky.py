e60a5733fbba272d1a45f2b853511ced316328fd
Nicholas Moore <nsmoore57@gmail.com>
2020-08-31 11:59:09 -0500

    Separate tests into functions - one per test

--- test_Cholesky.py
+++ test_Cholesky.py
@@ -7,23 +7,35 @@ import scipy.sparse as sp
 import Cholesky as Chol
 import pytest
 
-def test_CholeskyFactorization():
+def test_CholeskyFactorization_Correct():
     """Tests the Cholesky.CholeskyFactorization Method"""
-    tol = 1e-8
+    # tolerance is machine epsilon
+    tol = np.finfo(np.float64).eps
 
     n = 10
     ex = np.ones(n, dtype="double")
     data = np.array([-ex, 2*ex, -ex])
     offsets = np.array([1,0,-1])
-    A = sp.dia_matrix((data,offsets), shape=(n,n))
+    A = sp.diags([-1, 2, -1], [-1, 0, 1], shape=(n,n), dtype="d", format="dia")
 
     L = Chol.CholeskyFactorization(A)
-    assert LA.norm(L*L.T - A, ord=np.inf) < tol
+    # Check if the relative error is less than machine epsilon
+    assert LA.norm(L*L.T - A, ord=np.inf)/LA.norm(A,ord=np.inf) < tol
 
-    A = sp.dok_matrix(A)
+def test_CholeskyFactorization_Convert():
+    tol = np.finfo(np.float64).eps
+
+    n = 10
+    ex = np.ones(n, dtype="double")
+    data = np.array([-ex, 2*ex, -ex])
+    offsets = np.array([1,0,-1])
+    A = sp.diags([-1, 2, -1], [-1, 0, 1], shape=(n,n), dtype="d", format="dok")
     L = Chol.CholeskyFactorization(A)
-    assert LA.norm(L*L.T - A, ord=np.inf) < tol
+    # Check if the relative error is less than machine epsilon
+    assert LA.norm(L*L.T - A, ord=np.inf)/LA.norm(A,ord=np.inf) < tol
 
+def test_CholeskyFactorization_Symmetric():
+    offsets = np.array([1,0,-1])
     main_diag = 4.0*np.ones(4, dtype="double")
     sup_diag = np.array([0, 1, 2, 1], dtype="double")
     sub_diag = np.ones(4, dtype="double")
@@ -34,6 +46,8 @@ def test_CholeskyFactorization():
         L = Chol.CholeskyFactorization(A)
     assert "Matrix must be symmetric" in str(excinfo.value)
 
+def test_CholeskyFactorization_Definite():
+    offsets = np.array([1,0,-1])
     main_diag = 4.0*np.ones(4, dtype="double")
     main_diag[2] = 0.5
     sup_diag = np.ones(4, dtype="double")
@@ -44,6 +58,8 @@ def test_CholeskyFactorization():
         L = Chol.CholeskyFactorization(A)
     assert "Matrix must be positive definite" in str(excinfo.value)
 
+def test_CholeskyFactorization_Tridiagonal():
+    offsets = np.array([1,0,-1])
     main_diag = 4.0*np.ones(4, dtype="double")
     sup_diag = np.ones(4, dtype="double")
     data = np.vstack((sup_diag, main_diag, sup_diag))
@@ -55,6 +71,8 @@ def test_CholeskyFactorization():
         L = Chol.CholeskyFactorization(A)
     assert "Matrix must be tridiagonal" in str(excinfo.value)
 
+def test_CholeskyFactorization_Square():
+    offsets = np.array([1,0,-1])
     main_diag = 4.0*np.ones(5, dtype="double")
     sup_diag = np.ones(5, dtype="double")
     data = np.vstack((sup_diag, main_diag, sup_diag))
