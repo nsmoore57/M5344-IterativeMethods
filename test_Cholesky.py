2d8d7eae1e8297a5e95e9cee835855bf4e8801af
Nicholas Moore <nsmoore57@gmail.com>
2020-08-31 10:03:29 -0500

    Improve Tests

--- test_Cholesky.py
+++ test_Cholesky.py
@@ -2,26 +2,27 @@
 """This script tests the exported functions in Cholesky.py"""
 
 import numpy as np
-import numpy.linalg as LA
+import scipy.sparse.linalg as LA
 import scipy.sparse as sp
-# import Cholesky as Chol
+import Cholesky as Chol
+import pytest
 
-def test_Barrier_EqualityOnly():
-    """Tests the IP.InteriorPointBarrier_EqualityOnly Method"""
+def test_CholeskyFactorization():
+    """Tests the Cholesky.CholeskyFactorization Method"""
     tol = 1e-15
 
     n = 10
     ex = np.ones(n, dtype="double")
     data = np.array([-ex, 2*ex, -ex])
     offsets = np.array([1,0,-1])
-    A_dia = sp.dia_matrix((data,offsets), shape=(n,n))
+    A = sp.dia_matrix((data,offsets), shape=(n,n))
 
-    # L = Chol.Cholesky(A_dia)
-    # assert LA.norm(np.matmul(L, L.T) - A_dia, ord=inf) < tol
+    L = Chol.CholeskyFactorization(A)
+    # assert LA.norm(L*L.T - A, ord=np.inf) < tol
 
-    A_dok = sp.dok_matrix(A_dia)
-    # L = Chol.Cholesky(A_dok)
-    # assert LA.norm(np.matmul(L, L.T) - A_dia, ord=inf) < tol
+    A = sp.dok_matrix(A)
+    L = Chol.CholeskyFactorization(A)
+    # assert LA.norm(L*L.T - A, ord=np.inf) < tol
 
     main_diag = 4.0*np.ones(4, dtype="double")
     sup_diag = np.array([0, 1, 2, 1], dtype="double")
@@ -30,7 +31,7 @@ def test_Barrier_EqualityOnly():
     A = sp.dia_matrix((data, offsets), shape=(4,4))
 
     # with pytest.raises(RuntimeError) as excinfo:
-    #     L = Chol.Cholesky(A)
+    #     L = Chol.CholeskyFactorization(A)
     #     assert "Matrix must be symmetric" in str(excinfo.value)
 
     main_diag = 4.0*np.ones(4, dtype="double")
@@ -40,8 +41,8 @@ def test_Barrier_EqualityOnly():
     A = sp.dia_matrix((data, offsets), shape=(4,4))
 
     # with pytest.raises(RuntimeError) as excinfo:
-    #     L = Chol.Cholesky(A)
-    #     assert "Matrix must be positive definite" in str(excinfo.value)
+    #     L = Chol.CholeskyFactorization(A)
+    # assert "Matrix must be positive definite" in str(excinfo.value)
 
     main_diag = 4.0*np.ones(4, dtype="double")
     sup_diag = np.ones(4, dtype="double")
@@ -50,15 +51,17 @@ def test_Barrier_EqualityOnly():
     A[1,3] = A[3,1] = 1.0
     A = sp.dia_matrix(A)
 
-    # with pytest.raises(RuntimeError) as excinfo:
-    #     L = Chol.Cholesky(A)
-    #     assert "Matrix must be tridiagonal" in str(excinfo.value)
+    with pytest.raises(RuntimeError) as excinfo:
+        L = Chol.CholeskyFactorization(A)
 
-    main_diag = 4.0*np.ones(4, dtype="double")
-    sup_diag = np.ones(4, dtype="double")
+    assert "Matrix must be tridiagonal" in str(excinfo.value)
+
+    main_diag = 4.0*np.ones(5, dtype="double")
+    sup_diag = np.ones(5, dtype="double")
     data = np.vstack((sup_diag, main_diag, sup_diag))
-    A = sp.dia_matrix((data, offsets), shape=(4,4)).toarray()
+    A = sp.dia_matrix((data, offsets), shape=(4,5))
 
-    # with pytest.raises(RuntimeError) as excinfo:
-    #     L = Chol.Cholesky(A)
-    #     assert "Matrix must be square" in str(excinfo.value)
+    with pytest.raises(RuntimeError) as excinfo:
+        L = Chol.CholeskyFactorization(A)
+
+    assert "Matrix must be square" in str(excinfo.value)
