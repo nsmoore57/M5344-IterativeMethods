d2d415da7cc00087c249f5fcc6f938c5cc54b997
Nicholas Moore <nsmoore57@gmail.com>
2020-10-07 19:57:52 -0500

    MGPres - Better Jacobi Analysis

--- MultigridPresentation/MultigridPresentation.jl
+++ MultigridPresentation/MultigridPresentation.jl
@@ -4,75 +4,110 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ 0c8ddd80-0764-11eb-3076-f57c55c75d89
-begin 
+# ╔═╡ 4e6859e8-07ec-11eb-3200-5d7441db3541
+begin
 	import Pkg
 	Pkg.activate(".")
-	using FFTW
-	using AbstractFFTs
-	using Plots
 	using IterativeSolvers
 	using SparseArrays
-	using DSP
+	using Plots
+	using LinearAlgebra
 end
 
-# ╔═╡ 994548e0-0768-11eb-12cd-21726bbca62f
-begin
-	N = 100
-	xs = 1/(N+1)
-	x0 = 0
-	xmax = 1.0
-end
+# ╔═╡ f8619160-07ec-11eb-28eb-e12b3f552197
+md"# Multigrid"
 
-# ╔═╡ 3e1df2d0-076a-11eb-0145-33853c41168d
-begin
-	x = xs:xs:xmax-xs
-	wvnums = [2, 5, 10, 15]
-	y = zeros(length(x))
-	for w in wvnums
-		global y
-		y += sin.(2*π*w*x)
-	end
-	plot(y)
-end
+# ╔═╡ b2f84422-08da-11eb-15b0-ef0e45dab69c
+md"## Analyzing the Jacobi Method"
 
-# ╔═╡ 62930d90-0773-11eb-090a-df83c32cfc62
-function plotSpectrum(x, y)
-	F = fft(y) |> fftshift
-	freqs = AbstractFFTs.fftfreq(length(x), 1.0/xs) |> fftshift
-	freq_domain = plot(freqs, abs.(F), title = "Spectrum", xlim=(0, +50)) 
-	plot(freq_domain)
-end
+# ╔═╡ d4dc33ee-07ec-11eb-0bfd-039484b81720
+md"First, create the A matrix, this will correspond to a 1D poisson problem."
 
-# ╔═╡ f2ab3880-0769-11eb-06d6-a947829b1f94
-begin
-	plotSpectrum(x, y)
-	vline!(wvnums)
-end
+# ╔═╡ 268fd020-08e3-11eb-04b5-37f4a48a1e6e
+N = 1000
 
-# ╔═╡ b0e96f90-0762-11eb-2619-8de476bf1279
-begin
-	sub_diag = [-1 for i in 1:(N-1)]
-	main_diag = [2 for i in 1:N]
-	A = spdiagm(-1 => sub_diag, 0 => main_diag, 1 => sub_diag)
-end
+# ╔═╡ bedc360e-08da-11eb-29e8-45bc78190706
+A = spdiagm(-1=>-ones(N-1), 0=>2*ones(N), 1=>-ones(N-1))
 
-# ╔═╡ e9bdb7d0-0768-11eb-2332-a5947e1baa3f
+# ╔═╡ 03b38c4e-07ed-11eb-2200-1b381ca5c349
+md"For the sake of demonstration, we let the right-hand side vector b correspond to the case of Dirichlet boundary conditions where both ends are held at 0. We choose this because the true solution is then the zero vector and so the error cooresponds with the current iterate."
+
+# ╔═╡ 640fb614-07eb-11eb-25e9-c1501af3e6bb
 b = zeros(N)
 
-# ╔═╡ 7d86a930-0774-11eb-0b55-bb5b6f8ff278
-xinit = rand(N)
+# ╔═╡ 38a285ec-08db-11eb-3dbf-e52720123bb3
+md"Let's generate some initial guesses (which are also initial errors) which are sine waves with varying frequencies"
 
-# ╔═╡ 3c0a2270-0774-11eb-2f5d-2b3d7793b81d
-jacobi!(xinit, A, b)
+# ╔═╡ d2e7080e-08da-11eb-1280-4de82702e49d
+begin
+	x = range(0,stop=1, length=N)
+	waveNumbers = [1, 3, 7, 20, 100]
+	xinitial = [sin.(w*π*x) for w in waveNumbers]
+end
+
+# ╔═╡ 2846866c-08db-11eb-3287-5784dac4644b
+plot(x, xinitial, layout=(length(xinitial),1), legend=false)
+
+# ╔═╡ 2b55bab6-08dc-11eb-3f22-21afeaa4ea44
+numJacobiIters = 10000
+
+# ╔═╡ b9c1a3c4-08db-11eb-2e91-cb4241130b44
+md"Now let's run $numJacobiIters Jacobi iterations on each of these, tracking the error at each iteration."
+
+# ╔═╡ 2128b458-08dc-11eb-046e-97261b58dd8b
+begin
+	errors = [zeros(numJacobiIters) for i ∈ waveNumbers]
+	results = deepcopy(xinitial)
+	for j ∈ 1:numJacobiIters
+		for (i, w) ∈ enumerate(results)
+			errors[i][j] = norm(jacobi!(w, A, zeros(size(w)), maxiter=1))
+		end
+	end
+end
+
+# ╔═╡ 8e751486-08e4-11eb-377b-7b9169d0d51c
+md"Here we plot these errors"
+
+# ╔═╡ fac138a2-08de-11eb-1f75-39183f2801ec
+begin
+	labels = reshape([string(w) for w in waveNumbers], 1, :)
+	plot(errors, label=labels, xlabel="Iteration", ylabel="error")
+end
+
+# ╔═╡ f1915512-08e3-11eb-1e3f-cf0a3a44e916
+md"We can also plot the actual results of the iterations"
+
+# ╔═╡ 016ca266-08e4-11eb-18c6-09736686a7e2
+plot(x, results, layout=(length(xinitial),1), legend=false)
+
+# ╔═╡ 96db8162-08e3-11eb-36ad-6d583ec2234f
+md"Here we see the key to understanding the effectiveness of multigrid. The Jacobi iteration scheme is much better at eliminating high frequency error than low frequency error."
+
+# ╔═╡ c5ec024a-07ec-11eb-3868-49c7229e0e7d
+md"## Appendix"
+
+# ╔═╡ 812b3720-08e3-11eb-012a-9b0beb675bfb
+md"Imports"
 
 # ╔═╡ Cell order:
-# ╠═0c8ddd80-0764-11eb-3076-f57c55c75d89
-# ╠═994548e0-0768-11eb-12cd-21726bbca62f
-# ╠═3e1df2d0-076a-11eb-0145-33853c41168d
-# ╠═62930d90-0773-11eb-090a-df83c32cfc62
-# ╠═f2ab3880-0769-11eb-06d6-a947829b1f94
-# ╠═b0e96f90-0762-11eb-2619-8de476bf1279
-# ╠═e9bdb7d0-0768-11eb-2332-a5947e1baa3f
-# ╠═7d86a930-0774-11eb-0b55-bb5b6f8ff278
-# ╠═3c0a2270-0774-11eb-2f5d-2b3d7793b81d
+# ╟─f8619160-07ec-11eb-28eb-e12b3f552197
+# ╟─b2f84422-08da-11eb-15b0-ef0e45dab69c
+# ╟─d4dc33ee-07ec-11eb-0bfd-039484b81720
+# ╠═268fd020-08e3-11eb-04b5-37f4a48a1e6e
+# ╟─bedc360e-08da-11eb-29e8-45bc78190706
+# ╟─03b38c4e-07ed-11eb-2200-1b381ca5c349
+# ╟─640fb614-07eb-11eb-25e9-c1501af3e6bb
+# ╟─38a285ec-08db-11eb-3dbf-e52720123bb3
+# ╠═d2e7080e-08da-11eb-1280-4de82702e49d
+# ╟─2846866c-08db-11eb-3287-5784dac4644b
+# ╟─b9c1a3c4-08db-11eb-2e91-cb4241130b44
+# ╠═2b55bab6-08dc-11eb-3f22-21afeaa4ea44
+# ╠═2128b458-08dc-11eb-046e-97261b58dd8b
+# ╟─8e751486-08e4-11eb-377b-7b9169d0d51c
+# ╟─fac138a2-08de-11eb-1f75-39183f2801ec
+# ╟─f1915512-08e3-11eb-1e3f-cf0a3a44e916
+# ╟─016ca266-08e4-11eb-18c6-09736686a7e2
+# ╟─96db8162-08e3-11eb-36ad-6d583ec2234f
+# ╟─c5ec024a-07ec-11eb-3868-49c7229e0e7d
+# ╟─812b3720-08e3-11eb-012a-9b0beb675bfb
+# ╠═4e6859e8-07ec-11eb-3200-5d7441db3541
